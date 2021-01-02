c0000f06bb5a65c9f17d81833f1e4d13f068c7cc
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2021-01-02 20:21:05 +0600

    function with typeScript

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
+let x = " This is a String";
+let y = 900;
+x = " this different value";
+let test = String(y);
+console.log(typeof (test));
 alert("Hi I am Learning Typescript");
+// How to Deal with Function & Array
+function print(value) {
+    alert(value);
+}
+const myConst = " TypeScript Function ";
+print(myConst);
+// Pass Array 
+function ShowingNumber(value) {
+    for (let x = 0; x < value.length; x++) {
+        alert(value[x]);
+    }
+}
+let myArray = ["one", "two", "three"];
+ShowingNumber(myArray);
+//# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -1,1 +1,33 @@
+let x : string = " This is a String";
+
+let y : number = 900;
+
+x = " this different value";
+
+let test = String(y)
+console.log(typeof(test))
+
 alert("Hi I am Learning Typescript")
+
+
+// How to Deal with Function & Array
+
+function print(value : string) {
+    alert(value);
+}
+
+const myConst = " TypeScript Function ";
+
+print(myConst);
+
+// Pass Array 
+
+function ShowingNumber(value : Array<string>) {
+    for (let x = 0; x<value.length; x++) {
+        alert(value[x])
+    }
+}
+
+let myArray = [ "one", "two", "three"]
+
+ShowingNumber(myArray);
